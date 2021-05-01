e7baf0494c1930ceeff8e35d07296220db656421
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-05-01 18:50:19 +0900

    Initial implementation

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -20,27 +20,23 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/nak3/metadata-webhook/pkg/defaults"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/injection/sharedmain"
-	"knative.dev/pkg/logging"
-	"knative.dev/pkg/metrics"
 	"knative.dev/pkg/signals"
 	"knative.dev/pkg/system"
 	"knative.dev/pkg/webhook"
 	"knative.dev/pkg/webhook/certificates"
-	"knative.dev/pkg/webhook/configmaps"
 	"knative.dev/pkg/webhook/resourcesemantics"
 	"knative.dev/pkg/webhook/resourcesemantics/defaulting"
 	"knative.dev/pkg/webhook/resourcesemantics/validation"
-
-	"knative.dev/sample-controller/pkg/apis/samples/v1alpha1"
 )
 
 var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
-	// List the types to validate.
-	v1alpha1.SchemeGroupVersion.WithKind("AddressableService"): &v1alpha1.AddressableService{},
+	appsv1.SchemeGroupVersion.WithKind("Deployment"): &defaults.TargetDeployment{},
 }
 
 var callbacks = map[schema.GroupVersionKind]validation.Callback{}
@@ -69,50 +65,6 @@ func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher
 	)
 }
 
-func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
-	return validation.NewAdmissionController(ctx,
-
-		// Name of the resource webhook.
-		fmt.Sprintf("validation.webhook.%s.knative.dev", system.Namespace()),
-
-		// The path on which to serve the webhook.
-		"/resource-validation",
-
-		// The resources to validate.
-		types,
-
-		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
-		func(ctx context.Context) context.Context {
-			// Here is where you would infuse the context with state
-			// (e.g. attach a store with configmap data)
-			return ctx
-		},
-
-		// Whether to disallow unknown fields.
-		true,
-
-		// Extra validating callbacks to be applied to resources.
-		callbacks,
-	)
-}
-
-func NewConfigValidationController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
-	return configmaps.NewAdmissionController(ctx,
-
-		// Name of the configmap webhook.
-		fmt.Sprintf("config.webhook.%s.knative.dev", system.Namespace()),
-
-		// The path on which to serve the webhook.
-		"/config-validation",
-
-		// The configmaps to validate.
-		configmap.Constructors{
-			logging.ConfigMapName(): logging.NewConfigFromConfigMap,
-			metrics.ConfigMapName(): metrics.NewObservabilityConfigFromConfigMap,
-		},
-	)
-}
-
 func main() {
 	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
 		ServiceName: "webhook",
@@ -123,7 +75,5 @@ func main() {
 	sharedmain.WebhookMainWithContext(ctx, "webhook",
 		certificates.NewController,
 		NewDefaultingAdmissionController,
-		NewValidationAdmissionController,
-		NewConfigValidationController,
 	)
 }
--- go.mod
+++ go.mod
@@ -1,16 +1,15 @@
-module knative.dev/sample-controller
+module github.com/nak3/metadata-webhook
 
 go 1.15
 
 require (
+	github.com/google/go-cmp v0.5.5
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.16.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/api v0.19.7
 	k8s.io/apimachinery v0.19.7
-	k8s.io/client-go v0.19.7
 	k8s.io/code-generator v0.19.7
 	k8s.io/kube-openapi v0.0.0-20200805222855-6aeccd4b50c6
 	knative.dev/hack v0.0.0-20210428122153-93ad9129c268
-	knative.dev/hack/schema v0.0.0-20210428122153-93ad9129c268
 	knative.dev/pkg v0.0.0-20210428141353-878c85083565
 )
